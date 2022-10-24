91725e977557023f047727679998f3f7f20a2d4a
Rishabh Jain <73517462+1-Rishabh-Jain-1@users.noreply.github.com>
2022-10-24 11:08:59 +0530

    Update Bubblesort.py

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,1 +1,9 @@
-#
+def bubble_sort(arr):
+  for i in range(len(arr)):
+    for j in range(len(arr) - i - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+arr = list(map(int, input("Enter elements for array: ").strip().split()))
+bubble_sort(arr)
+print(arr)
